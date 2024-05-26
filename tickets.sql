eb6b2c438c8da13ffdc5ef87c238de7bc06acd32
Kapil2508 <99133992+Kapil2508@users.noreply.github.com>
2024-05-26 18:42:55 +0530

    Add files via upload

---
+++ tickets.sql
@@ -0,0 +1,8 @@
+create database springboot8;
+use springboot8;
+
+show tables;
+desc tickets;
+
+insert into tickets(ticketTitle,ticketShortDescription,ticketCreatedOn)values("ABCD","happy","23-05-2024");
+select * from tickets;
